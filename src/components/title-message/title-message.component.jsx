a5626e63aa35f42d8a66c3c84e13fa7d4ad8be1c
Enyabuto <elishahyperx@yahoo.com>
2020-12-15 22:45:34 -0600

    Carousel,Carousel caption,Scroll down

---
+++ src/components/title-message/title-message.component.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Typewriter from "typewriter-effect";
+import styled from "styled-components";
+
+const MyTitleMessage = styled.h1`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  width: 100%;
+  top: 22rem;
+  z-index: 1;
+  margin-top: -125px;
+  text-align: center;
+  strong {
+    font-size: 1.25em;
+  }
+  div {
+    color: ${props => props.theme.textColor};
+    text-shadow: 0px 2px 2px rgba(255, 255, 255, 0.4);
+    font-weight: 100;
+    letter-spacing: 7px;
+    .main {
+      font-size: 70px;
+    }
+    .sub {
+      font-size: 57px;
+      letter-spacing: 2px;
+    }
+  }
+`;
+
+
+const TitleMessage = () => {
+    return (
+        <MyTitleMessage>
+             <div className="titleMessage">
+                <div className="heading">
+                    <div className="main text-center mb-3">
+                        Hi, I am
+                        <br />
+                        <span>
+                            <strong>ELisha Osoro Nyabuto</strong>
+                        </span>
+                    </div>
+                    <div className="sub">
+                        <Typewriter
+                            options={{
+                            strings: ["Front End Dev + Mobile Development", "Learner", "Gamer"],
+                            autoStart: true,
+                            loop: true,
+                            delay: 50
+                            }}
+                        />
+                    </div>
+                </div>
+             </div>   
+        </MyTitleMessage>
+    )
+}
+
+export default TitleMessage
