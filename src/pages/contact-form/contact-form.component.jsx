1d2792c97b6f1084b2e6fa089c4ce61143649349
Enyabuto <elishahyperx@yahoo.com>
2021-03-09 18:36:55 -0600

    Contact Page

--- src/pages/contact-form/contact-form.component.jsx
+++ src/pages/contact-form/contact-form.component.jsx
@@ -5,51 +5,66 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import "./contact-form.styles.css";
 
-
-
-
 const ContactForm = () => {
-    return (
-      <div id="contact">
-        <h1 className="pt-3 text-center font-details-b pb-3">CONTACT ME</h1>
-        <Jumbotron className="contact-jumbotron">
-          <Row>
-            <Col className="d-flex justify-content-center flex-wrap">
-              <div className="m-2">
-                <a href="mailto:elisha462twitter@gmail.com" target="_blank" rel="noopener noreferrer">
-                  <Button variant="outline-danger" title="elisha462twitter@gmail.com">
-                    <i className="fas fa-envelope-square"></i> Email Me
-                  </Button>
-                </a>
-              </div>
-              <div className="m-2">
-                <a href="https://www.linkedin.com/in/elisha-nyabuto-1636661bb/" target="_blank" rel="noopener noreferrer">
-                  <Button variant="outline-primary" title="Visit my LinkenIn">
-                    <i className="fab fa-linkedin"></i> LinkedIn
-                  </Button>
-                </a>
-              </div>
-              
-              <div className="m-2">
-                <a href="https://github.com/Enyabuto" target="_blank" rel="noopener noreferrer">
-                  <Button variant="outline-dark" title="My projects">
-                    <i className="fab fa-github-square"></i> GitHub
-                  </Button>
-                </a>
-              </div>
-              <div className="m-2">
-                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-                  <Button variant="outline-primary" title="Say hello on FB">
-                    <i className="fab fa-facebook-square"></i> FaceBook
-                  </Button>
-                </a>
-              </div>
-            </Col>
-          </Row>
-        </Jumbotron>
-      </div>
-    );
-  };
-  
-  export default ContactForm;
-  
+  return (
+    <div id="contact">
+      <h1 className="pt-3 text-center font-details-b pb-3">CONTACT ME</h1>
+      <Jumbotron className="contact-jumbotron">
+        <Row>
+          <Col className="d-flex justify-content-center flex-wrap">
+            <div className="m-2">
+              <a
+                href="mailto:elishaonyabuto@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button
+                  variant="outline-danger"
+                  title="elishaonyabuto@gmail.com"
+                >
+                  <i className="fas fa-envelope-square"></i> Email Me
+                </Button>
+              </a>
+            </div>
+            <div className="m-2">
+              <a
+                href="https://www.linkedin.com/in/elisha-nyabuto-1636661bb/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant="outline-primary" title="Visit my LinkenIn">
+                  <i className="fab fa-linkedin"></i> LinkedIn
+                </Button>
+              </a>
+            </div>
+
+            <div className="m-2">
+              <a
+                href="https://github.com/Enyabuto"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant="outline-dark" title="My projects">
+                  <i className="fab fa-github-square"></i> GitHub
+                </Button>
+              </a>
+            </div>
+            <div className="m-2">
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant="outline-primary" title="Say hello on FB">
+                  <i className="fab fa-facebook-square"></i> FaceBook
+                </Button>
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default ContactForm;
